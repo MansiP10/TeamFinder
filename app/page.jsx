59ddf29c3ecc8e0a83d0cb9928a236546c351641
MansiP10 <mansi.prajapati101998@gmail.com>
2025-01-26 21:57:03 +0530

    fixed quests issue3

--- app/page.jsx
+++ app/page.jsx
@@ -49,9 +49,7 @@ const Home = () => {
           <h1 className="text-7xl font-extrabold gradient-title pb-6">
             Find Your Perfect Team <span className="text-orange">for Success</span>
           </h1>
-          <p className="text-xl text-gray-600 mb-10">
-          Join a community of skilled individuals ready to collaborate on hackathons challenges and group projects. Connect with experts in various fields to enhance your team capabilities.
-        </p>
+          <p className="text-xl text-gray-600 mb-10">Join a community of skilled individuals.</p>
           <Link href={"/dashboard"}>
             <Button size="lg" className="text-lg">
               Get Started <ArrowRight className="ml-2 h-5 w-5" />
